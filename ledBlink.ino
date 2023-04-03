8afd5fd5889dd8994b1ae58a97844841288efa05
KalseyC <100498824+KalseyColotl@users.noreply.github.com>
2023-04-02 23:10:44 -0400

    added blinking

--- ledBlink.ino
+++ ledBlink.ino
@@ -6,10 +6,15 @@ int ledPin2 = 14;
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW);
+  digitalWrite(ledPin2, HIGH);
+  delay(500);
+  digitalWrite(ledPin2, LOW);
 }
